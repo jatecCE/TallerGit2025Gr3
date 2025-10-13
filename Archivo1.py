667ee8e6f8c243534eb885249173ad9b7b75006e
joscCE <josc4188@gmail.com>
2025-10-13 16:34:59 -0600

    suma

--- Archivo1.py
+++ Archivo1.py
@@ -2,9 +2,12 @@ def imprimeNombre():
     print("Hola Jason")
 
 def addDescription():
-    print("Este es un taller de demostración")
+    print("Este es un taller de demostración, o no?")
 
 
 def imprimir_jose():
     print("hola jose :v")
 
+def sumar_dos_num(a, b):
+    print("trolleado")
+    return a - b 
